1a770f240595b566952d3630057ecfd1e7ed7c70
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-10-19 23:04:41 +0300

    Lesson2 start. Testing Mutex

--- TryDontDie/Program.cs
+++ TryDontDie/Program.cs
@@ -1,41 +1,18 @@
 ﻿using System;
 using System.Collections;
 
-Random random = new Random();
-
-
-
-
-//for (int i = 0; i < 5; i++)
+//Mutex mtx = new Mutex();
+//for (int i = 0; i < 10; i++)
 //{
-//    Thread thr = new Thread(new ThreadStart(()=>CountMethod(i)));
+//    Thread thr = new Thread(new ThreadStart(RunThread));
 //    thr.Start();
 //}
-//Thread.Sleep(1000);
-//Console.WriteLine("Stop");
-//void CountMethod(int threadId)
+
+//void RunThread()
 //{
-//    lock (lockObject)
-//    {
-//        count = 1;
-//        for (int i = 0; i < 10; i++)
-//        {
-//            Console.WriteLine($"{count} in {threadId}");
-//            count++;
-//            Thread.Sleep(250);
-//        }
-//    }
+//    mtx.WaitOne();
+//    Console.WriteLine($"thread {Thread.CurrentThread.ManagedThreadId} start");
+//    Thread.Sleep(1000);
+//    Console.WriteLine($"thread {Thread.CurrentThread.ManagedThreadId} stop");
+//    mtx.ReleaseMutex();
 //}
-////Thread thr = new Thread(new ThreadStart(Method));
-////thr.Start();
-////Thread.Sleep(9000);
-////Console.WriteLine("Stop");
-
-
-////    void Method(){
-////	for (int i = 0; i < 1000; i++)
-////	{
-////		Console.WriteLine(i);
-////		Thread.Sleep(250);
-////	}
-////}
