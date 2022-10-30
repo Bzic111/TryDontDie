7da29b309e44392fc2d51f68c382883d3881ea64
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-10-30 11:46:15 +0300

    MaxSize exception added

--- MicroThreadPull/ObjectPool.cs
+++ MicroThreadPull/ObjectPool.cs
@@ -1,23 +1,26 @@
-﻿
-
-
-
-public sealed class ObjectPool<TPullItem> where TPullItem : PullItem, new()
+﻿public sealed class ObjectPool<TPullItem> where TPullItem : PullItem, new()
 {
     private readonly Queue<TPullItem> _queue = new();
     public int PoolCount { get => _queue.Count; }
+    public int MaxSize { get; private set; } = 0;
     public TPullItem Get()
     {
         return _queue.Count > 0 ? _queue.Dequeue() : new TPullItem();
     }
-
+    public void SetMaxSize(int size) => MaxSize = size;
     public void Release(TPullItem item)
     {
         if (item is null)
             return;
         item.Reset();
         item.Id = item.Id == 0 ? _queue.Count + 1 : item.Id;
-
-        _queue.Enqueue(item);
+        if (MaxSize==0 || _queue.Count< MaxSize)
+        {
+            _queue.Enqueue(item);
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException("Reached maximum size of pool...");
+        }
     }
 }
