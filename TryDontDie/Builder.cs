7022b018d7b8044f8f37dd6bd3c2ec50f3a4cf21
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-10-30 14:35:05 +0300

    Builder

---
+++ TryDontDie/Builder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryDontDie
+{
+    public interface IPrintDevice
+    {
+
+    }
+    public interface IReport
+    {
+        void Print(IPrintDevice printDevice);
+    }
+    internal sealed class BigReport : IReport
+    {
+        private readonly IList<string> _blocks = new List<string>();
+
+        public void AddBlockInfo(string info)
+        {
+            _blocks.Add(info);
+        }
+        public void Print(IPrintDevice printDevice)
+        {
+
+        }
+    }
+    public sealed class ReportBuilder
+    {
+        private BigReport _report;
+        public void AddHeader()
+        {
+            _report.AddBlockInfo("Adding Header");
+        }
+        public void AddFooter()
+        {
+            _report.AddBlockInfo("Adding footer");
+        }
+
+        public void AddBody()
+        {
+            _report.AddBlockInfo("Adding body");
+        }
+        public void AddExInfo(string info)
+        {
+            _report.AddBlockInfo(info);
+        }
+        public void Reset()
+        {
+            _report = new BigReport();
+        }
+        public IReport BuildReport()
+        {
+            BigReport report = _report;
+            Reset();
+            return report;
+        }
+    }
+}
--- TryDontDie/Program.cs
+++ TryDontDie/Program.cs
@@ -1,67 +1,4 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;public interface IBill
-{
-    long Id { get; }
-}
-public abstract class BillManagament
-{
-    protected abstract IBill CreateBill();
-    public IBill RegisterNewBill()
-    {
-        IBill bill = CreateBill();
-        bool result = CommitBill(bill);
-        if (result)
-        {
-            Console.WriteLine($"Success to commit bill with id {bill.Id}");
-            return bill;
-        }
-        throw new Exception("Failed to commit bill");
-    }
-    protected abstract bool CommitBill(IBill bill);
-}
-internal sealed class SalesBill : IBill
-{
-    private long _id;
-    public long Id
-    {
-        get => _id * 100;
-        set => _id = value;
-    }
-}
-internal sealed class FinanceBill:IBill
-{
-    private long _id;
-    public long Id
-    {
-        get => _id * 1000;
-        set => _id = value;
-    }
-}
-public sealed class SalesBillManagement : BillManagament
-{
-    protected override bool CommitBill(IBill bill)
-    {
-        Console.WriteLine($"Bill {bill.Id} saved");
-        return true;
-    }
-
-    protected override IBill CreateBill()
-    {
-        return new SalesBill();
-    }
-}
-public sealed class FinanceBillManagement : BillManagament
-{
-    protected override bool CommitBill(IBill bill)
-    {
-        Console.WriteLine($"Bill {bill.Id} saved");
-        return true;
-    }
-
-    protected override IBill CreateBill()
-    {
-        return new FinanceBill();
-    }
-}
+using System.Linq;
