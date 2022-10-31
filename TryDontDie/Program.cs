425cd4c00625c7699a855bf611ef53554b828aaf
GavrilovIY <GavrilovIY@IVANB3V>
2022-10-31 13:20:36 +0300

    HW lesson3.1

--- TryDontDie/Program.cs
+++ TryDontDie/Program.cs
@@ -8,52 +8,71 @@ using System.Linq;
 с почасовой оплатой (один из потомков) и фиксированной оплатой (второй потомок).
 а) Описать в базовом классе абстрактный метод для расчёта среднемесячной заработной платы.
 Для «повременщиков» формула для расчета такова: 
-«среднемесячная заработная плата = 20.8 * 8 * почасовая ставка», 
-
+«среднемесячная заработная плата = 20.8 * 8 * почасовая ставка»,
 для работников с фиксированной оплатой
 «среднемесячная заработная плата = фиксированная месячная оплата».
  */
 
+Worker specialist = HumanResources.GetSpecialist("Alexandr", "Alexandrov", "Alexandrovich", 1000);
+Worker temporaryWorker = HumanResources.GetTemporaryWorker("Ivan", "Ivanov", "Ivanovich", 10);
+
+System.Console.WriteLine(specialist.Salary.Calculate());
+System.Console.WriteLine(temporaryWorker.Salary.Calculate());
+Console.ReadKey(false);
+
 public interface ISalary
 {
-    decimal Calculate();
-}
-public interface IPerHourSalary : ISalary
-{
-    decimal Calculate();
-}
-public interface IPerMonthSalary : ISalary
-{
+    decimal BaseSalary { get; set; }
     decimal Calculate();
 }
 
-abstract class PerHourSalary:ISalary
+public class PerHourSalary : ISalary
 {
-    decimal PerHourPayment { get; set; }
-    public decimal Calculate()
-    {
-        return (decimal)20.8 * 8 * PerHourPayment;
-    }
+    public decimal BaseSalary { get; set; }
+    public decimal Calculate() => (decimal)20.8 * 8 * BaseSalary;
 }
-abstract class FixedSalary:ISalary
+public class FixedSalary : ISalary
 {
-    decimal FixedAmount { get; set; }
-    public decimal Calculate() => FixedAmount;
+    public decimal BaseSalary { get; set; }
+    public decimal Calculate() => BaseSalary;
 }
 
 public abstract class Worker
 {
-    int id { get; set; }
-    string Name { get; set; }
-    string LastName { get; set; }
-    string Patronimic { get; set; }
-    decimal Salary { get; set; }
-    protected abstract decimal Payment();
-}
-public class WorkerPayPerHour : Worker
-{
-    protected override decimal Payment()
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Patronimic { get; set; }
+    public ISalary Salary { get; set; }
+    public Worker(int id, string name, string lastName, string patronimic, ISalary salary)
     {
-        throw new NotImplementedException();
+        Id = id;
+        Name = name;
+        LastName = lastName;
+        Patronimic = patronimic;
+        Salary = salary;
+    }
+}
+public class Specialist : Worker
+{
+    public Specialist(int id, string name, string lastName, string patronimic, decimal salary)
+    : base(id, name, lastName, patronimic, new FixedSalary() { BaseSalary = salary }) { }
+
+}
+public class TemporaryWorker : Worker
+{
+    public TemporaryWorker(int id, string name, string lastName, string patronimic, decimal salary)
+    : base(id, name, lastName, patronimic, new PerHourSalary() { BaseSalary = salary }) { }
+}
+public class HumanResources
+{
+    private static int Counter = 1;
+    public static Worker GetSpecialist(string name, string lastName, string patronimic, decimal salary)
+    {
+        return new Specialist(Counter++, name, lastName, patronimic, salary);
+    }
+    public static Worker GetTemporaryWorker(string name, string lastName, string patronimic, decimal salary)
+    {
+        return new TemporaryWorker(Counter++, name, lastName, patronimic, salary);
     }
 }
