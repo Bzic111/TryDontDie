51e82efed54559408cc34bd01a7fff615c3eb212
GavrilovIY <GavrilovIY@IVANB3V>
2022-10-31 14:56:57 +0300

    Урок 3 завершено

--- BuilderHW3.1/Program.cs
+++ BuilderHW3.1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using BuilderForWorker.Classes;
 using BuilderForWorker.Interfaces;
 using System;
-
+using System.Text;
 /*
  1. Построить три класса (базовый и 2 потомка), описывающих некоторых работников 
 с почасовой оплатой (один из потомков) и фиксированной оплатой (второй потомок).
@@ -10,6 +10,8 @@ using System;
 «среднемесячная заработная плата = 20.8 * 8 * почасовая ставка»,
 для работников с фиксированной оплатой
 «среднемесячная заработная плата = фиксированная месячная оплата».
+б) Создать на базе абстрактного класса массив сотрудников и заполнить его,
+вывести список сотрудников на экран.
  */
 
 Worker specialist = HumanResources.GetSpecialist("Alexandr", "Alexandrov", "Alexandrovich", 1000);
@@ -17,4 +19,25 @@ Worker temporaryWorker = HumanResources.GetTemporaryWorker("Ivan", "Ivanov", "Iv
 
 Console.WriteLine(specialist.Salary.Calculate());
 Console.WriteLine(temporaryWorker.Salary.Calculate());
+List<Worker> workers = new();
+for (int i = 0; i < 10; i++)
+{
+    workers.Add(HumanResources.GetSpecialist(GetRandomString(), GetRandomString(), GetRandomString(), i * 100));
+    workers.Add(HumanResources.GetTemporaryWorker(GetRandomString(), GetRandomString(), GetRandomString(), i * 10));
+}
+
+foreach (var item in workers)
+{
+    System.Console.WriteLine($"{item.Id}\t{item.Name}\t{item.Patronimic}\t{item.LastName}\t{item.Salary.Calculate()}");
+}
+
 Console.ReadKey(false);
+string GetRandomString()
+{
+    Random rnd = new Random();
+    StringBuilder sb = new();
+    for (int i = 0; i < 8; i++)
+        sb.Append((char)rnd.Next('a', 'z'));
+
+    return sb.ToString();
+}
